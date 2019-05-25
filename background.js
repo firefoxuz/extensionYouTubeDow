6c6a312c4ea4ffbad4bcd5adea8c12f66f31a251
iamhackeddeveloper <daler.sultonov.00@gmail.com>
2019-05-25 12:01:13 +0500

    Last version

---
+++ background.js
@@ -0,0 +1,9 @@
+function onExtensionClick(tab) {
+  if (tab.url.indexOf("youtube.com") != -1) {
+    chrome.tabs.create({
+      url: tab.url.replace("youtube.com", "ssyoutube.com")
+    });
+  }
+}
+
+chrome.browserAction.onClicked.addListener(onExtensionClick);
